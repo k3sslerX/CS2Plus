fe78fa5422fca411da67e60918f49b183f426518
k3ssler <jkessler365@mail.ru>
2025-04-13 13:58:41 +0300

    structure reformat

--- main.go
+++ main.go
@@ -9,15 +9,4 @@ func main() {
 	if err == nil {
 		cs2plus.GetOverallStats(user)
 	}
-	//// Вывод K/D, убийств, смертей и т. д.
-	//for _, stat := range data.Playerstats.Stats {
-	//	switch stat.Name {
-	//	case "total_kills":
-	//		fmt.Printf("Убийства: %d\n", stat.Value)
-	//	case "total_deaths":
-	//		fmt.Printf("Смерти: %d\n", stat.Value)
-	//	case "total_mvps":
-	//		fmt.Printf("MVP: %d\n", stat.Value)
-	//	}
-	//}
 }
