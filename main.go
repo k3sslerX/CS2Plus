0e00d94197cec0206955708177d15d68719a4d54
k3ssler <jkessler365@mail.ru>
2025-04-16 14:42:51 +0300

    added faceit overall stats parser

--- main.go
+++ main.go
@@ -4,7 +4,32 @@ import (
 	"cs2plus/statsFaceit"
 	"cs2plus/statsValve"
 	"fmt"
+	"strings"
 )
+
+func PrintFaceitStats(stats *statsFaceit.FaceitStats) {
+	// Создаем красивую таблицу с выравниванием
+	header := "FACEIT STATISTICS"
+	separator := strings.Repeat("=", 40)
+
+	fmt.Printf("\n%s\n%s\n", header, separator)
+
+	// Форматируем float значения с 2 знаками после запятой
+	fmt.Printf("%-25s %15.2f%%\n", "Winrate:", stats.Winrate)
+	fmt.Printf("%-25s %15d\n", "Total Kills:", stats.TotalKills)
+	fmt.Printf("%-25s %15d\n", "Total Headshots:", stats.TotalHeadshots)
+	fmt.Printf("%-25s %15d\n", "Total Assists:", stats.TotalAssists)
+	fmt.Printf("%-25s %15d\n", "Total Deaths:", stats.TotalDeaths)
+	fmt.Printf("%-25s %15d\n", "Total MVPs:", stats.TotalMVPs)
+	fmt.Printf("%-25s %15d\n", "Avg Kills per Match:", stats.AvgKills)
+	fmt.Printf("%-25s %15d\n", "Avg Headshots per Match:", stats.AvgHeadshots)
+	fmt.Printf("%-25s %15d\n", "Avg Assists per Match:", stats.AvgAssists)
+	fmt.Printf("%-25s %15d\n", "Avg MVPs per Match:", stats.AvgMVPs)
+	fmt.Printf("%-25s %15.2f\n", "Avg K/D Ratio:", stats.AvgKD)
+	fmt.Printf("%-25s %15.2f\n", "Avg K/R Ratio:", stats.AvgKR)
+	fmt.Printf("%-25s %15.2f\n", "Average ADR:", stats.ADR)
+	fmt.Println(separator)
+}
 
 func main() {
 	user, err := statsValve.FindUser("k3ssler")
@@ -12,9 +37,10 @@ func main() {
 		faceitUser, err := statsFaceit.GetFaceitPlayer(user.SteamID64)
 		if err == nil {
 			statistics, err := statsFaceit.GetFaceitLast20MatchesStats(faceitUser)
-			fmt.Println(err)
 			if err == nil {
-				fmt.Println(statistics)
+				PrintFaceitStats(statistics)
+			} else {
+				fmt.Println(err)
 			}
 		}
 
--- statsFaceit/lastStats.go
+++ statsFaceit/lastStats.go
@@ -1,12 +1,16 @@
 package statsFaceit
 
+// RESULT 0 - L / 1 - W
+
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	"github.com/joho/godotenv"
 	"io"
 	"net/http"
 	"os"
+	"strconv"
 )
 
 func init() {
@@ -25,13 +29,44 @@ func getApiKey() (string, error) {
 	return faceitAPIKey, nil
 }
 
-type MapStats struct {
-	MapName string
-	Wins    int
-	Losses  int
+type FaceitStats struct {
+	Winrate        float64
+	TotalKills     int
+	TotalHeadshots int
+	TotalAssists   int
+	TotalDeaths    int
+	TotalMVPs      int
+	AvgKills       int
+	AvgHeadshots   int
+	AvgAssists     int
+	AvgMVPs        int
+	AvgKD          float64
+	AvgKR          float64
+	ADR            float64
 }
 
-func GetFaceitLast20MatchesStats(user *FaceitUser) ([]MapStats, error) {
+type MatchStats struct {
+	Kills     string `json:"Kills"`
+	Headshots string `json:"Headshots"`
+	Assists   string `json:"Assists"`
+	Deaths    string `json:"Deaths"`
+	MVPs      string `json:"MVPs"`
+	ADR       string `json:"ADR"`
+	KRRatio   string `json:"K/R Ratio"`
+	Result    string `json:"Result"`
+}
+
+type Item struct {
+	Stats MatchStats `json:"stats"`
+}
+
+type Response struct {
+	Items []Item `json:"items"`
+	Start int    `json:"start"`
+	End   int    `json:"end"`
+}
+
+func GetFaceitLast20MatchesStats(user *FaceitUser) (*FaceitStats, error) {
 	url := fmt.Sprintf("https://open.faceit.com/data/v4/players/%s/games/cs2/stats", user.FaceitID)
 
 	req, err := http.NewRequest("GET", url, nil)
@@ -52,7 +87,41 @@ func GetFaceitLast20MatchesStats(user *FaceitUser) ([]MapStats, error) {
 	}
 	defer resp.Body.Close()
 	body, err := io.ReadAll(resp.Body)
-	fmt.Println(string(body))
+	var response Response
+	err = json.Unmarshal(body, &response)
+	if err != nil {
+		return nil, err
+	}
+	stats := FaceitStats{}
+	adrs := 0.0
+	krs := 0.0
+	wins := 0
+	for _, item := range response.Items {
+		kills, _ := strconv.Atoi(item.Stats.Kills)
+		headshots, _ := strconv.Atoi(item.Stats.Headshots)
+		assists, _ := strconv.Atoi(item.Stats.Assists)
+		deaths, _ := strconv.Atoi(item.Stats.Deaths)
+		mvps, _ := strconv.Atoi(item.Stats.MVPs)
+		stats.TotalKills += kills
+		stats.TotalHeadshots += headshots
+		stats.TotalAssists += assists
+		stats.TotalDeaths += deaths
+		stats.TotalMVPs += mvps
 
-	return nil, nil
+		adr, _ := strconv.ParseFloat(item.Stats.ADR, 64)
+		adrs += adr
+		kr, _ := strconv.ParseFloat(item.Stats.KRRatio, 64)
+		krs += kr
+		win, _ := strconv.Atoi(item.Stats.Result)
+		wins += win
+	}
+	stats.AvgKills = stats.TotalKills / len(response.Items)
+	stats.AvgHeadshots = stats.TotalHeadshots / len(response.Items)
+	stats.AvgAssists = stats.TotalAssists / len(response.Items)
+	stats.AvgMVPs = stats.TotalMVPs / len(response.Items)
+	stats.AvgKD = float64(stats.TotalKills) / float64(stats.TotalDeaths)
+	stats.AvgKR = krs / float64(len(response.Items))
+	stats.ADR = adrs / float64(len(response.Items))
+	stats.Winrate = (float64(wins) / float64(len(response.Items))) * 100.0
+	return &stats, nil
 }
